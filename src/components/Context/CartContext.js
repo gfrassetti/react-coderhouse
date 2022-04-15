3fd2f7b2be418bfc2b3f1a2cd25735da2032c4c5
Guido Frassetti <78879523+gfrassetti@users.noreply.github.com>
2022-04-15 16:20:27 -0300

    total price

--- src/components/Context/CartContext.js
+++ src/components/Context/CartContext.js
@@ -1,37 +1,36 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
-  console.log(cartProducts);
+  console.log(totalPrice);
 
   const addProductToCart = (product) => {
     if (cartProducts.find((cartProduct) => cartProduct.id === product.id)) {
       cartProducts.map((cartProduct) => {
         cartProduct.quantity++;
       });
-      caluclateTotalPrice();
     } else {
       setCartProducts((cartProducts) => [...cartProducts, product]);
-      caluclateTotalPrice();
     }
   };
   const deleteProduct = (product) => {
     setCartProducts(
       cartProducts.filter((cartProduct) => cartProduct.id !== product.id)
     );
-    caluclateTotalPrice();
   };
-  const caluclateTotalPrice = () => {
+
+  useEffect(() => {
     setTotalPrice(
       cartProducts.reduce(
-        (acc, cartProduct) => (acc = cartProduct.quantity * cartProduct.price),
+        (acc, cartProduct) =>
+          (acc = acc + cartProduct.quantity * cartProduct.price),
         0
       )
     );
-  };
+  }, [cartProducts]);
 
   const data = {
     cartProducts,
