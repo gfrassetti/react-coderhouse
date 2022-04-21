eed9582d0e24b5a7dda646e6e007ca058ecc6954
Guido Frassetti <78879523+gfrassetti@users.noreply.github.com>
2022-04-20 21:52:41 -0300

    chekout

--- src/components/ChekoutPage/CheckoutPage.js
+++ src/components/ChekoutPage/CheckoutPage.js
@@ -1,10 +1,6 @@
 import React, { useContext, useState } from "react";
-import CartContext from "../Context/CartContext";
 
 const CheckoutPage = () => {
-  const { cartProducts, deleteProduct, totalPrice } = useContext(CartContext);
-
-
   return (
     <React.Fragment>
       <h2>Pagina de Chekout</h2>
